18f47cc79f10944ae916d1b7334b5d05db6bec0c
ZhengPei1 <zhengpei.pz@gmail.com>
2024-05-24 21:30:03 -0400

    style update on multiple components, start writing the trade page, no bug at this point

--- client/src/app/trade/page.js
+++ client/src/app/trade/page.js
@@ -1,12 +1,45 @@
 "use client"
 import Navbar from "@/components/Navbar"
+import styles from "./page.module.css"
 
-export default function Trade(){
-    return(
+export default function Trade() {
+    return (
         <main className="main" >
             <div className="navbar"><Navbar> </Navbar></div>
-            <div className="main_panel">
-                <h1 style={{"textAlign" : "center"}}>Under Development</h1>
+            <div className={`main_panel ${styles.container}`}>
+
+                <div className={styles.info}>
+                    <div>Account Balance (USD)</div>
+                    <button>Reset</button>
+                    <div>Equity</div>
+                    <div>Commission</div>
+                </div>
+
+
+                <div className={styles.watchlist}>
+                    <div className={styles.title}>Watchlist</div>
+                    <div>Symbol</div>
+                    <div>Last</div>
+                    <div>Chg</div>
+                    <div>Chg%</div>
+                    <div>Exit Pt</div>
+                </div>
+
+                <div className={styles.trending}>
+                    <div className={styles.title}>Trending</div>
+                </div>
+
+                <div className={styles.trade}>
+                    <div>Symbol</div>
+                    <div>Last</div>
+                    <div>Chg</div>
+                    <div>Chg%</div>
+                    <div>Exit Pt</div>
+                    <div>Purchase</div>
+                    <div>Sell</div>
+                </div>
+
+
             </div>
         </main>
     )
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,28 +5,38 @@ import { useRouter } from 'next/navigation'
 import { useContext } from "react"
 import { AuthContext } from "@/util/AuthContext"
 
-export default function Navbar(){
+export default function Navbar() {
     const router = useRouter();
     const [user, loading, setLoading] = useContext(AuthContext);
 
-    const handleLogOut = async(e) =>{
-        try{
+    const handleLogOut = async (e) => {
+        try {
             // set loading user state to true to prevent profile page rendering error
-            setLoading(true); 
+            setLoading(true);
             router.push("/"); // redirect before logout
             await logOut();
-            
-        }catch(error){
+
+        } catch (error) {
             alert("logout failed");
         }
     }
 
     return (
         <div className={styles.navbar}>
-            <Link href="/home" ><img title="home screen" src="/stock_info.svg"/></Link>
+            <label className={styles.label}>Home</label>
+            <Link href="/home" ><img title="home screen" src="/stock_info.svg" /></Link>
+
+            <label className={styles.label}>Trade</label>
             <Link href="/trade"><img title="trade stock" src="/trade.svg" /></Link>
+
+            <label className={styles.label}>Profile</label>
             <Link href="/profile"><img title="my profile" src="/profile.svg" /></Link>
-            <img title="log out" src="/logout.svg" onClick={handleLogOut}/>
+
+            <label className={styles.label}>Log Out</label>
+            <img title="log out"
+                src="/logout.svg"
+                className={styles.img}
+                onClick={handleLogOut}/>
         </div>
     )
 }
